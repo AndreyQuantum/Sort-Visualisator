211d10921f7b13143712ba73d4129b5c034502f9
AndreyQuantum <zelentsov1254@gmail.com>
2024-04-28 00:47:30 +0200

    + BaseSort abstract class + bubbleSort algorythm + Bubble sort props

---
+++ src/main/java/com/azelentsov/sortVisualisator/SortAlgorythms/BaseSort.java
@@ -0,0 +1,81 @@
+package com.azelentsov.sortVisualisator.SortAlgorythms;
+
+import lombok.extern.java.Log;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Random;
+
+public abstract class BaseSort{
+
+    protected int[] arrayToSort;
+
+
+    private Map<String, String> properties;
+
+    protected abstract void getNext();
+
+    public BaseSort(int lengthOfArray) {
+        populateArray(lengthOfArray);
+        String name = this.getClass().getSimpleName();
+        String pathToFile = getPathToProperties(name);
+        Properties props = readPropsFile(pathToFile, name);
+        properties = propertiesToMap(props);
+    }
+
+    private static String getPathToProperties(String name) {
+        var path = Paths.get(System.getProperty("user.dir"),  "src", "main", "java", "com", "azelentsov",
+                "sortVisualisator", "SortAlgorythms","properties", name + ".properties");
+        return path.toString();
+    }
+
+    private Map<String, String> propertiesToMap(Properties props) {
+        Map<String, String> propertiesMap =  new HashMap<>();
+        props.forEach((key, value) -> propertiesMap.put((String) key, (String) value));
+        return propertiesMap;
+    }
+
+    private Properties readPropsFile(String filePath, String name){
+        var res = new Properties();
+        res.setProperty("name", name);
+        try (FileInputStream in = new FileInputStream(filePath)) {
+            res.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    protected Map<String,String> getProperties(){
+        return properties;
+    }
+
+    protected String getName(){
+        return properties.get("name");
+    }
+
+    protected void populateArray(int length){
+        arrayToSort = new int[length];
+        Random random = new Random(12345);
+        for (int i = 0; i<= length-1; i++){
+            arrayToSort[i] = random.nextInt(0,100);
+        }
+    }
+    protected void populateArray(int length, int maxNumber){
+        arrayToSort = new int[length];
+        Random random = new Random(12345);
+        for (int i = 0; i<= length-1; i++){
+            arrayToSort[i] = random.nextInt(0,maxNumber);
+        }
+    }
+
+    protected void swap(int indexA, int indexB){
+        int temp = arrayToSort[indexA];
+        arrayToSort[indexA] = arrayToSort[indexB];
+        arrayToSort[indexB] = temp;
+    }
+}
---
+++ src/main/java/com/azelentsov/sortVisualisator/SortAlgorythms/BubbleSort.java
@@ -0,0 +1,53 @@
+package com.azelentsov.sortVisualisator.SortAlgorythms;
+
+public class BubbleSort extends BaseSort {
+
+
+    public BubbleSort(int lengthOfArray) {
+        super(lengthOfArray);
+
+    }
+
+
+
+    @Override
+    protected void getNext() {
+        optimized();
+    }
+
+    private void standart(){
+        for (int endPointer = arrayToSort.length; endPointer>1; endPointer--){
+            for (int currentIndex=1; currentIndex< endPointer; currentIndex++){
+                if (arrayToSort[currentIndex-1] > arrayToSort[currentIndex]){
+                    swap(currentIndex-1, currentIndex);
+                }
+            }
+        }
+    }
+    private void optimized(){
+        for (int endPointer = arrayToSort.length; endPointer>1; endPointer--){
+            boolean isNotSwapped = true;
+            for (int currentIndex=1; currentIndex< endPointer; currentIndex++){
+                if (arrayToSort[currentIndex-1] > arrayToSort[currentIndex]){
+                    swap(currentIndex-1, currentIndex);
+                    isNotSwapped = false;
+                }
+            }
+            if (isNotSwapped){
+                break;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        var sort = new BubbleSort(3);
+        System.out.println(sort.getProperties());
+//        System.out.println("Element count \t elapsed time");
+//        for (int N = 10; N<=1_000_000; N*=10){
+//            var bubbleTest = new BubbleSort(N);
+//            bubbleTest.run();
+//        }
+    }
+
+}
