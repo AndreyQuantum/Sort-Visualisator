860591d5c3f80accfeaa2835bab0d5217dba61cb
AndreyQuantum <zelentsov1254@gmail.com>
2024-05-18 07:51:10 +0200

    + BubbleSort optimised

--- src/main/java/com/azelentsov/sortVisualisator/Core/sortAlgorithms/BubbleSortOptimised.java
+++ src/main/java/com/azelentsov/sortVisualisator/Core/sortAlgorithms/BubbleSortOptimised.java
@@ -7,24 +7,28 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class BubbleSort extends BaseSort {
+public class BubbleSortOptimised extends BaseSort {
 
     @Override
     protected void run() {
-        standart();
+        optimized();
     }
 
-    private void standart(){
+    private void optimized(){
         for (int endPointer = listToSort.size(); endPointer>1; endPointer--){
-            for (int currentIndex=1; currentIndex < endPointer; currentIndex++){
+            boolean isNotSwapped = true;
+            for (int currentIndex=1; currentIndex< endPointer; currentIndex++){
                 saveArrayBeforeIteration();
                 if (listToSort.get(currentIndex-1).value() > listToSort.get(currentIndex).value()){
                     swap(currentIndex-1, currentIndex);
+                    isNotSwapped = false;
                 }
                 saveArrayAfterIteration(new int[]{currentIndex, currentIndex - 1});
             }
+            if (isNotSwapped){
+                break;
+            }
         }
     }
-
 
 }
