0f57f505b96b52063beabf095c27686d51b6e654
AndreyQuantum <zelentsov1254@gmail.com>
2024-05-19 00:20:18 +0200

    Remove unused import in IterationResult
    The unused import statement, 'java.util.List', was removed from the IterationResult class to clean up the code and improve readability. There were no functionalities or dependencies associated with this import that would affect the behavior of the application.

--- src/main/java/com/azelentsov/sortVisualisator/Core/records/IterationResult.java
+++ src/main/java/com/azelentsov/sortVisualisator/Core/records/IterationResult.java
@@ -1,6 +1,5 @@
 package com.azelentsov.sortVisualisator.Core.records;
 
-import java.util.List;
 
 public record IterationResult(int[] indexesToFocusOn,
                               int[] arrayBeforeIteration,
