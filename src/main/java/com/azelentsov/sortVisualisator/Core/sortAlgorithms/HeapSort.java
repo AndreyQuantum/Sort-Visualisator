2a5acc6dbde4f37de512ba46e7d286cd3c0bdbb2
AndreyQuantum <zelentsov1254@gmail.com>
2024-05-24 03:03:22 +0200

    Add HeapSort algorithm to sortVisualisator
    This commit involves adding a new file HeapSort.java to the sortVisualisator application. HeapSort is an important and popular sorting algorithm, thus has been implemented in the file, complete with methods (`run`, `heapify`, `populateProps`) needed for the sorting visualisation. The properties of this algorithm have also been recorded, providing useful information about its performance and application in different scenarios.

---
+++ src/main/java/com/azelentsov/sortVisualisator/Core/sortAlgorithms/HeapSort.java
@@ -0,0 +1,62 @@
+package com.azelentsov.sortVisualisator.Core.sortAlgorithms;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HeapSort extends BaseSort{
+    @Override
+    protected void run() {
+        for (int root = listToSort.size()/2;root >= 0; root--){
+            heapify(root,listToSort.size());
+        }
+        for (int index = listToSort.size()-1; index > 0; index--){
+            saveArrayBeforeIteration();
+            highlightVariable(0, "0");
+            highlightVariable(index, "index");
+            swap(0, index);
+            saveArrayAfterIteration();
+            heapify(0, index);
+        }
+    }
+
+
+    private void heapify(int root, int end){
+        saveArrayBeforeIteration();
+        int max = root;
+        int L = (2*root)+1;
+        int R = (2*root)+2;
+        if (L < end && listToSort.get(L).value() > listToSort.get(max).value()){
+            max = L;
+        }
+        if (R < end && listToSort.get(R).value() > listToSort.get(max).value()) {
+            max = R;
+        }
+        if (max == root){
+            return;
+        }
+        displayVariable(root,"root");
+        displayVariable(L,"L");
+        displayVariable(R,"R");
+        highlightVariable(root,"root");
+        highlightVariable(L,"L");
+        highlightVariable(R,"R");
+        swap(root,max);
+        saveArrayAfterIteration();
+        heapify(max,end);
+    }
+    @Override
+    protected void populateProps() {
+        props.put("Time Complexity (Average)", "O(n log n)");
+        props.put("Time Complexity (Best)", "O(n log n)");
+        props.put("Time Complexity (Worst)", "O(n log n)");
+        props.put("Space Complexity", "O(1)");
+        props.put("Stable", "No");
+        props.put("In-Place", "Yes");
+        props.put("Adaptive", "No");
+        props.put("First Published", "1964");
+        props.put("Suitable For", "Large datasets, when in-place sorting is required");
+        props.put("Not Suitable For", "Datasets where stability is important");
+        props.put("Swaps", "O(n log n)");
+        props.put("Passes", "O(log n)");
+    }
+}
